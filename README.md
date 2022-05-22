f18633ecb26fe3e217cf057f89870b7ef2b4165a
sotto97_win <sotto.bw97@gmail.com>
2022-05-22 22:08:11 +0900

    ドロップダウンメニューの設定から行っていく

--- README.md
+++ README.md
@@ -22,3 +22,22 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+---
+## 初期セットアップはこれを参考に実施
+https://reffect.co.jp/vue/vuetify-for-beginner
+
+## エラー対処法
+* vuetify Component name "Home" should always be multi-word
+  * vue.config.jsに以下を記載する。
+  ```
+  const { defineConfig } = require('@vue/cli-service')
+  module.exports = defineConfig({
+    lintOnSave:false, // 追記
+    transpileDependencies: [
+      'vuetify'
+    ]
+  })
+  ```
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+	<v-app>
+		<v-navigation-drawer app v-model="drawer" clipped> Navigation Lists</v-navigation-drawer>
+		<v-app-bar color="primary" dark app clipped-left>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-toolbar-title>Vuetify</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-toolbar-items>
+				<v-btn text>Button</v-btn>
+				<v-btn text>Button</v-btn>
+			</v-toolbar-items>
+		</v-app-bar>
+		<v-footer color="primary" dark app>Vuetify</v-footer>
+	</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+	data() {
+		return {
+			drawer: null,
+		};
+	},
+};
+</script>
