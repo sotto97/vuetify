22bd2eaff7cd1ec7f87dc522725ecc8a4d4c2b5c
sotto97_win <sotto.bw97@gmail.com>
2022-05-23 22:13:01 +0900

    次はVuerouter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 <template>
 	<v-app>
-		<v-navigation-drawer app v-model="drawer" clipped> Navigation Lists</v-navigation-drawer>
+		<v-navigation-drawer app v-model="drawer" clipped>
+			<v-container>
+				<v-list-item>
+					<v-list-item-content>
+						<v-list-item-title class="title grey--text text-darken-2">
+							Navigation Lists
+						</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-divider></v-divider>
+				<v-list dense nav>
+					<v-list-group
+						v-for="nav_list in nav_lists"
+						:key="nav_list.name"
+						:prepend-icon="nav_list.icon"
+						no-action
+						:append-icon="nav_list.lists ? undefined : ''"
+					>
+						<template v-slot:activator>
+							<v-list-item-content>
+								<v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+							</v-list-item-content>
+						</template>
+						<v-list-item v-for="list in nav_list.lists" :key="list">
+							<v-list-item-content>
+								<v-list-item-title>{{ list }}</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</v-list>
+			</v-container>
+		</v-navigation-drawer>
 		<v-app-bar color="primary" dark app clipped-left>
 			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 			<v-toolbar-title>Vuetify</v-toolbar-title>
@@ -9,18 +40,25 @@
 				<v-btn text>For Enterprise</v-btn>
 				<v-menu offset-y>
 					<template v-slot:activator="{ on }">
-						<v-btn v-on="on" text>Support</v-btn>
+						<v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
 					</template>
 					<v-list>
-						<v-list-item v-for="support in supports" :key="support">
+						<v-subheader>Get help</v-subheader>
+						<v-list-item v-for="support in supports" :key="support.name">
+							<v-list-item-icon>
+								<v-icon>{{ support.icon }}</v-icon>
+							</v-list-item-icon>
 							<v-list-item-component>
-								<v-list-item-title>{{ support }}</v-list-item-title>
+								<v-list-item-title>{{ support.name }}</v-list-item-title>
 							</v-list-item-component>
 						</v-list-item>
 					</v-list>
 				</v-menu>
 			</v-toolbar-items>
 		</v-app-bar>
+		<v-main>
+			<router-view />
+		</v-main>
 		<v-footer color="primary" dark app>Vuetify</v-footer>
 	</v-app>
 </template>
@@ -31,11 +69,27 @@ export default {
 		return {
 			drawer: null,
 			supports: [
-				"Consulting and supprt",
-				"Discord community",
-				"Report a bug",
-				"Github issue board",
-				"Stack overview",
+				{ name: "Consulting and support", icon: "mdi-vuetify" },
+				{ name: "Discord community", icon: "mdi-discord" },
+				{ name: "Report a bug", icon: "mdi-bug" },
+				{ name: "Github issue board", icon: "mdi-github" },
+				{ name: "Stack overview", icon: "mdi-stack-overflow" },
+			],
+			nav_lists: [
+				{
+					name: "Getting Started",
+					icon: "mdi-vuetify",
+					lists: ["Quick Start", "Pre^made layouts"],
+				},
+				{ name: "Customization", icon: "mdi-cogs" },
+				{
+					name: "Styles & animations",
+					icon: "mdi-palette",
+					lists: ["Colors", "Content", "Display"],
+				},
+				{ name: "UI Components", icon: "mdi-view-dashboard", lists: ["API explorer", "Alerts"] },
+				{ name: "Directives", icon: "mdi-function" },
+				{ name: "Preminum themes", icon: "mdi-vuetify" },
 			],
 		};
 	},
